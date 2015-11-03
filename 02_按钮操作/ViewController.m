5cb0c4957073bf194c168404ba84b02321cc9502
MA LEI <apple@APPLEdeMacBook-Pro-46.local>
2015-11-03 16:04:39 +0800

    实现按钮操作

--- 02_按钮操作/ViewController.m
+++ 02_按钮操作/ViewController.m
@@ -10,18 +10,58 @@
 
 @interface ViewController ()
 
+@property (nonatomic,weak)  IBOutlet UIButton *iconButton;
+
+
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//- (void)viewDidLoad {
+//    [super viewDidLoad];
+//    // Do any additional setup after loading the view, typically from a nib.
+//}
+//
+//- (void)didReceiveMemoryWarning {
+//    [super didReceiveMemoryWarning];
+//    // Dispose of any resources that can be recreated.
+//}
+- (IBAction)top{
+    
+   // self.iconButton.frame.origin.y -=20;
+   //UIView中的属性frame是个CGRECT结构体，其中包括CGPoint结构体origin和一个CGSIZE的size
+    //origin中包含x和y，修改位置可以通过frame.origin.x和frame.origin.y来修改
+    //但在OC中不允许直接这么修改，要取出frame，修改完毕后再赋值回去
+    
+    CGRect frame=self.iconButton.frame;
+    frame.origin.y -=20;
+    self.iconButton.frame=frame;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (IBAction)right{
+    
+    CGRect frame=self.iconButton.frame;
+    frame.origin.x +=20;
+    self.iconButton.frame=frame;
 }
+
+- (IBAction)bottom{
+    
+    CGRect frame=self.iconButton.frame;
+    frame.origin.y +=20;
+    self.iconButton.frame=frame;
+}
+
+- (IBAction)left{
+    
+    CGRect frame=self.iconButton.frame;
+    frame.origin.x -=20;
+    self.iconButton.frame=frame;
+}
+
+
+
 
 @end
