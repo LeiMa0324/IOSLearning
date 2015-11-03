e31109797c22734280f1e8ad575dc726313d7811
MA LEI <apple@APPLEdeMacBook-Pro-46.local>
2015-11-03 22:43:41 +0800

    放大缩小和旋转

--- 02_按钮操作/ViewController.m
+++ 02_按钮操作/ViewController.m
@@ -36,7 +36,7 @@ typedef enum
 
 
 
-/*====================================================
+/*========================位移1（上下左右的代码）
 - (IBAction)top{
     
    // self.iconButton.frame.origin.y -=20;
@@ -73,32 +73,119 @@ typedef enum
 }
 */
 
-//=================代码优化
+//=================位移2（利用控件的tag属性和switch进行代码优化）
+
+//
+//-(IBAction)Move:(UIButton *)button
+//{
+//    CGRect frame=self.iconButton.frame;
+//    
+//    switch (button.tag) {
+//        case top:
+//            frame.origin.y -=20;
+//            break;
+//        case right:
+//            frame.origin.x +=20;
+//            break;
+//        case left:
+//            frame.origin.x -=20;
+//            break;
+//        case bottom:
+//            frame.origin.y +=20;
+//            break;
+//
+//    }
+//    
+//    self.iconButton.frame=frame;
+//
+//}
 
 
--(IBAction)Move:(UIButton *)button
+//=================位移的代码优化2（利用控件的transform属性和Transform方法进行位移）
+
+
+- (IBAction)Move:(UIButton *)button
 {
-    CGRect frame=self.iconButton.frame;
-    
-    switch (button.tag) {
-        case top:
-            frame.origin.y -=20;
-            break;
-        case right:
-            frame.origin.x +=20;
-            break;
-        case left:
-            frame.origin.x -=20;
-            break;
-        case bottom:
-            frame.origin.y +=20;
-            break;
 
+    //CGAffineTransform为系统定义的一个结构体.定义了四个CGFloat a,b,c,d和两个偏移量CGfloat tx和CGFloat ty，UIView中包含了transform这个属性
+
+    //1、将需要偏移的量存在CGAffineTransform类型的变量中
+    //此方法非实例方法或者类方法。y轴偏离-100，x不变
+    //2、将此偏移量赋值给目标frame
+   // self.iconButton.transform = CGAffineTransformMakeTranslation(0, -100);
+    //CGAffineTransformMakeTranslation只会针对原有位置进行改变，多次执行无效
+    CGFloat xDelta,yDelta;
+    
+    //如果按钮是上下键，则x偏移量不变，y改变
+    if (button.tag==0||button.tag==2) {
+        yDelta=(button.tag==0)?-20:20;
+        //如果tag是0，上键，y偏移-20，否则便偏移20
     }
     
-    self.iconButton.frame=frame;
-
+    if (button.tag==1||button.tag==3) {
+        xDelta=(button.tag==3)?-20:20;
+        //如果tag是3，左键，x偏移-20，否则便偏移20
+    }
+    
+    //修改icon的transform
+    //CGAffineTransformTranslate可以以原来的transform为原点，多次变换位置
+    self.iconButton.transform=CGAffineTransformTranslate(self.iconButton.transform, xDelta, yDelta);
 }
+
+
+
+
+//=================放大缩小1（使用控件的bounds属性进行放大缩小）
+
+//-(IBAction)Zoom:(UIButton *)buttton
+//{
+//    
+//    //与操作frame方法类似，只不过改变了bounds.size.width和height
+//    CGRect bounds=self.iconButton.bounds;
+//    if (buttton.tag) {
+//        bounds.size.width +=20;
+//        bounds.size.height +=20;
+//    }
+//    else
+//    {
+//        bounds.size.width -=20;
+//        bounds.size.height -=20;
+//    }
+//    
+//    //开始一个动画
+//    [UIView beginAnimations:nil context:nil];
+//    [UIView setAnimationDuration:1.0];  //设置动画时长
+//    
+//    self.iconButton.bounds=bounds;
+//    
+//    [UIView commitAnimations];
+//}
+
+//=================放大缩小2（使用控件的transform属性和Transform方法进行放大缩小）
+
+-(IBAction)Zoom:(UIButton *)buttton
+{
+    //如果button的tag为4，即加号，则scale放大为1.2
+    CGFloat scale=(buttton.tag==4)?1.2:0.8;
+    
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:1.0];
+    self.iconButton.transform=CGAffineTransformScale(self.iconButton.transform, scale, scale);
+    //后面两个参数是x与y放大的比例
+    
+    NSLog(@"%@",NSStringFromCGAffineTransform(self.iconButton.transform));
+    
+    [UIView commitAnimations];
+}
+
+- (IBAction)Rotate:(UIButton *)button {
+    
+    //在OC开发中，角度一律使用弧度制，逆时针是负值
+    //180度=M_PI
+    CGFloat angle=(button.tag==1)? M_PI_4:- M_PI_4;//向右旋转转正45度，向左转负45
+    self.iconButton.transform=CGAffineTransformRotate(self.iconButton.transform, angle);
+}
+
 
 
 
